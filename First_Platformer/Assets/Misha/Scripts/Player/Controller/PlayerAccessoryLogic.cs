9e25480dc2c91ee74284f2bd22a30a04eb6ddaa4
Smoky6263 <80895318+Smoky6263@users.noreply.github.com>
2024-10-04 17:59:41 +0300

    - поправил скрипты игрока.

--- First_Platformer/Assets/Misha/Scripts/Player/Controller/PlayerAccessoryLogic.cs
+++ First_Platformer/Assets/Misha/Scripts/Player/Controller/PlayerAccessoryLogic.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 
 public class PlayerAccessoryLogic : MonoBehaviour
-    {
-        [SerializeField] private SkinnedMeshRenderer m_renderer = null;
-        public SkinnedMeshRenderer Renderer { get { return m_renderer; } }
+{
+    [SerializeField] private SkinnedMeshRenderer m_renderer = null;
+    public SkinnedMeshRenderer Renderer { get { return m_renderer; } }
 
-        [SerializeField] private GameObject m_rig = null;
+    [SerializeField] private GameObject m_rig = null;
 
-        private void Awake() { Destroy(m_rig); }
-    }
+    private void Awake() { Destroy(m_rig); }
+}
--- First_Platformer/Assets/Misha/Scripts/Player/Controller/PlayerAccessoryWearLogic.cs
+++ First_Platformer/Assets/Misha/Scripts/Player/Controller/PlayerAccessoryWearLogic.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
+
 public class PlayerAccessoryWearLogic : MonoBehaviour
 {
     [SerializeField] private SkinnedMeshRenderer m_characterRenderer;
